b92baf25beafa6748ace68f6f38349bb159083c5
kixem <nicvalll@gmail.com>
2016-08-05 18:16:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+
+var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var mouseDown  ; 
+$(".controls").on("click","li", function(){
+  $(this).siblings().removeClass("selected");
+  $(this).addClass("selected");
+  color = $(this).css("background-color");
+});
+
+$("#revealColorSelect").click(function(){
+    changeColor();
+   $("#colorSelect").toggle(); 
+    
+});
+
+function changeColor() {
+    var r = $("#red").val();
+    var g = $("#green").val();
+    var b = $("#blue").val();
+    
+    $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+            
+}
+                       
+$("input[type=range]").change(changeColor);
+
+$("#addNewColor").click(function(){
+    var $newColor = $("<li></li>");
+    $newColor.css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").append($newColor);
+    
+});
+                       
+$canvas.mousedown(function(e){
+        lastEvent = e;
+    mouseDown = true;
+    context.beginPath();
+context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+context.lineTo(e.offsetX, e.offsetY);
+context.strokeStyle = color;
+context.stroke();
+}).mousemove(function(e){
+    if (mouseDown){
+context.beginPath();
+context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+context.lineTo(e.offsetX, e.offsetY);
+context.strokeStyle = color;
+context.stroke();
+    lastEvent = e;
+    }
+             }).mouseup(function(){
+    mouseDown = false;
+}).mouseleave(function(){
+    $canvas.mouseup();
+});
